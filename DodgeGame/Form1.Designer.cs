d10e0a6a1851ed205be5425c7324aa28754163a6
nithumas226 <nithumas226@outlook.com>
2021-06-04 12:30:25 -0400

    Add project files.

---
+++ DodgeGame/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace DodgeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(250, 218);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(124, 23);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.outputLabel);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dodge Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ DodgeGame/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DodgeGame
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player = new Rectangle(10, 293, 15, 15);
+        List<Rectangle> downRectangles = new List<Rectangle>();
+        List<Rectangle> upRectangles = new List<Rectangle>();
+
+        int playerSpeed = 10;
+        int downRectangleSpeed = 5;
+        int upRectangleSpeed = -5;
+        int upRectangleCounter = 0;
+        int downRectangleCounter = 0;
+
+        bool rightDown = false;
+        bool leftDown = false;
+        bool upDown = false;
+        bool downDown = false;
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Rectangle leftRectangles = new Rectangle(190, 0, 20, 60);
+            downRectangles.Add(leftRectangles);
+            Rectangle rightRectangles = new Rectangle(490, 540, 20, 60);
+            upRectangles.Add(rightRectangles);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+                case Keys.Up:
+                    upDown = true;
+                    break;
+                case Keys.Down:
+                    downDown = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+                case Keys.Up:
+                    upDown = false;
+                    break;
+                case Keys.Down:
+                    downDown = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //Move Player
+            if (leftDown == true && player.X > 0)
+            {
+                player.X -= playerSpeed;
+            }
+            
+            if (rightDown == true && player.X < this.Width - player.Width)
+            {
+                player.X += playerSpeed;
+            }
+            
+            if (downDown == true && player.Y < this.Height - player.Height)
+            {
+                player.Y += playerSpeed;
+            }
+
+            if (upDown == true && player.Y > 0)
+            {
+                player.Y -= playerSpeed;
+            }
+
+            //Moving Rectangles
+            for (int i = 0; i < downRectangles.Count(); i++)
+            {
+                int y = downRectangles[i].Y + downRectangleSpeed;
+                downRectangles[i] = new Rectangle(downRectangles[i].X, y, 20, 60);
+            }
+
+            for (int i = 0; i < upRectangles.Count(); i++)
+            {
+                int y = upRectangles[i].Y + upRectangleSpeed;
+                upRectangles[i] = new Rectangle(upRectangles[i].X, y, 20, 60);
+            }
+
+            //Adding Rectangles according to counter
+            downRectangleCounter++;
+
+            for (int i = 0; i < downRectangles.Count(); i++)
+            {
+                if (downRectangleCounter == 22)
+                {
+                    downRectangles.Add(new Rectangle(190, 0, 20, 60));
+                    downRectangleCounter = 0;
+                }
+            }
+
+            upRectangleCounter++;
+
+            for (int i = 0; i < upRectangles.Count(); i++)
+            {
+                if (upRectangleCounter == 22)
+                {
+                    upRectangles.Add(new Rectangle(490, 540, 20, 60));
+                    upRectangleCounter = 0;
+                }
+            }
+
+            //Check to see if player touches rectangles or edge of form and stop game
+            for (int i = 0; i < downRectangles.Count(); i++)
+            {
+                if (player.IntersectsWith(downRectangles[i]))
+                {
+                    gameTimer.Enabled = false;
+                    outputLabel.Text = "You Lose!!";
+                }
+            }
+
+            for (int i = 0; i < upRectangles.Count(); i++)
+            { 
+                if (player.IntersectsWith(upRectangles[i]))
+                {
+                    gameTimer.Enabled = false;
+                    outputLabel.Text = "You Lose!!";
+                }
+            }
+
+            if (player.X > this.Width - player.Width)            
+            {
+                gameTimer.Enabled = false;
+                outputLabel.Text = "You Win!!";
+            }
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(whiteBrush, player);
+            for (int i = 0; i < downRectangles.Count(); i++)
+            {
+                e.Graphics.FillRectangle(blackBrush, downRectangles[i]);
+                e.Graphics.FillRectangle(blackBrush, upRectangles[i]);
+            }
+             
+
+        }
+    }
+}
